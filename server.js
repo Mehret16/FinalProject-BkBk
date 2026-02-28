bcbfac4e7c2c3b27380c271bc67fe649cfe71cbc
Abyssinia06 <abyssiniadereje97@gmail.com>
2026-02-28 18:13:17 +0300

    deploy stage

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ import chatRoutes from './routes/chatRoutes.js';
 
 const app = express();
 app.use(cors({
-   origin:['http://localhost:3000', 'https://mental-health-safespace.netlify.app/'],
+   origin:[ 'https://mental-health-safespace.netlify.app','http://localhost:5174','http://localhost:3000','http://localhost:5175',  
+     'http://localhost:5173',   
+     'http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
