41cfcb05d5ae08860e044a18cda4efe09ddf42d8
Abyssinia06 <abyssiniadereje97@gmail.com>
2026-03-07 12:01:50 +0300

    vercel diployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import chatRoutes from './routes/chatRoutes.js';
 
 const app = express();
 app.use(cors({
-   origin:[ 'https://mental-health-safespace.netlify.app','http://localhost:5174','http://localhost:3000','http://localhost:5175',  
+   origin:[ 'Mental Health Support Chatbot','http://localhost:5174','http://localhost:3000','http://localhost:5175',  
      'http://localhost:5173',   
      'http://localhost:3000'],
     credentials: true,
